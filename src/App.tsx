da15c317cc84def3ff04e6583fdf6eb13951299a
Tamerlan.Pirmammadov <tamerlan.pirmammadov@guavapay.com>
2024-11-16 17:54:49 +0400

    new changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,8 @@
 export default function App() {
-  return <h1 className='text-3xl font-bold underline'>Tailwind setup</h1>;
+  return (
+    <div>
+      <h1 className='text-3xl font-bold underline'>Tailwind setup</h1>
+      <p>new workflow</p>
+    </div>
+  );
 }
